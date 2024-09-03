bf94fe29a519e43dfa74cf9a40bcefc2c0bc1412
Luis Quiñones <lpaandres2020@gmail.com>
2024-09-02 22:41:29 -0600

    feat: professional experience description can be a complex data structure

--- src/types/professional-experience.ts
+++ src/types/professional-experience.ts
@@ -1,3 +1,11 @@
+export class Description {
+  public achievements: string[];
+
+  constructor(inputs: { achievements: string[] }) {
+    this.achievements = inputs.achievements;
+  }
+}
+
 export interface IProfessionalExperience {
   company: string;
   position: string;
@@ -5,5 +13,5 @@ export interface IProfessionalExperience {
   mode: string;
   since: string;
   until: string;
-  description: string;
+  description: string | Description;
 }
